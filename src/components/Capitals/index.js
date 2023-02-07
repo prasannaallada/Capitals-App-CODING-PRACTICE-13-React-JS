b9c17380093d56dd715ef86e627def2b763da512
prasannaallada <prasannaallada9@gmail.com>
2023-02-07 17:36:41 +0000

    capital app

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,148 @@
+import {Component} from 'react'
+
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+// Write your code here
+
+class Capitals extends Component {
+  state = {
+    activeCapitalId: countryAndCapitalsList[0].id,
+  }
+
+  onChangeCapital = event => {
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+
+    const activeCountryAndCapital = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+
+    return activeCountryAndCapital.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+    const country = this.getCountry(activeCapitalId)
+
+    return (
+      <div className="app-container">
+        <div className="capitals-container">
+          <h1 className="heading">Countries And Capitals</h1>
+          <div className="question-container">
+            <select
+              className="capital-select"
+              onChange={this.onChangeCapital}
+              value={activeCapitalId}
+            >
+              {countryAndCapitalsList.map(eachCapital => (
+                <option
+                  key={eachCapital.id}
+                  value={eachCapital.id}
+                  className="option"
+                >
+                  {eachCapital.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="question">is capital of which country?</p>
+          </div>
+          <p className="country">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Capitals
+
+/*
+console.log(countryAndCapitalsList)
+
+class Capitals extends Component {
+  state = {
+    activeCapitalId: countryAndCapitalsList[0].id,
+  }
+
+  onChangeCapital = event => {
+    this.setState({activeCapitalId: event.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+    console.log(activeCapitalId)
+
+    const activeCountryAndCapital = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+
+    return activeCountryAndCapital.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+    const country = this.getCountry(activeCapitalId)
+
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="heading">Countries And Capitals</h1>
+          <div className="select-container">
+            <select
+              className="input-element"
+              id="inputElement"
+              value={activeCapitalId}
+              onChange={this.onChangeCapital}
+            >
+              {countryAndCapitalsList.map(eachCaptial => (
+                <option
+                  className="option"
+                  key={eachCaptial.id}
+                  value={eachCaptial.id}
+                >
+                  {eachCaptial.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="label" htmlFor="inputElement">
+              is capital of which country?
+            </p>
+          </div>
+          <p className="text">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+export default Capitals
+*/
